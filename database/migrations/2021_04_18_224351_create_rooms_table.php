61a313664b52caa5ce7401dd5a0bb5354a603142
saiful29 <shaifulbd123@gmail.com>
2021-04-19 05:07:56 +0600

    basic

---
+++ database/migrations/2021_04_18_224351_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->unsignedBigInteger('shift_id');
+            $table->foreign('shift_id')->references('id')->on('shifts');
+            $table->unsignedBigInteger('section_id');
+            $table->foreign('section_id')->references('id')->on('sections');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
---
+++ database/migrations/2021_04_18_224435_create_students_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('registration_no', 20);
+            $table->string('room_roll', 5);
+            $table->string('class_roll', 5);
+            $table->unsignedBigInteger('room_id');
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->string('nick_name',20);
+            $table->string('first_name',20);
+            $table->string('last_name',20);
+            $table->string('father_name',40);
+            $table->string('mother_name',40);
+            $table->string('father_number',20);
+            $table->string('mother_number',20);
+            $table->string('date_of_birth',20);
+            $table->text('address');
+            $table->string('image',50);
+            $table->string('status',2);
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
